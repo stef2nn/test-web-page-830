44f27d44116460e060867b76025b33f260b518a2
stef2nn <61807101+stef2nn@users.noreply.github.com>
2020-06-12 09:13:29 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,10 +1,11 @@
 <?php
-if(){//show data
-
+if(isset($_GET["FirstName"])){//show data
+  echo $_GET["FirstName"];
 }else{//show form
   echo '
   <form action="">
-  
+  <p>First Name: <input type="text" name="FirstName" /></p>
+  <p><input type="submit" /></p>
   </form>
   ';
 }
